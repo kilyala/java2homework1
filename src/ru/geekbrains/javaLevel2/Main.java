d8e2fdbd7b8b437f90370ed6c00d3da92b0561c4
kilyala <herasav@gmail.com>
2021-04-04 21:22:56 +0300

    finally

--- src/ru/geekbrains/javaLevel2/Main.java
+++ src/ru/geekbrains/javaLevel2/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		Cat cat = new Cat("Tom", 100, 10);
 		Robot robot = new Robot("T800", 1000, 15);
 		Track track = new Track(150);
-		Wall wall = new Wall(8);
+		Wall wall = new Wall(10);
 
 		Object[] objects = {human, cat, robot};
 		Object[] events = {track, wall};
@@ -17,17 +17,54 @@ public class Main {
 			System.out.println("Выполняет " + objects[i].getClass().getSimpleName());
 			for (int j = 0; j < events.length; j++) {
 				System.out.println("Пробует " + events[j].getClass().getSimpleName());
-				if (events[j] instanceof Wall) {
-					if (((Human) objects[i]).jump(((Wall) events[j]).wallHeight)) {
-						System.out.println("Смог");
+				if (objects[i] instanceof Human) {
+					if (events[j] instanceof Wall) {
+						if (((Human) objects[i]).jump(((Wall) events[j]).wallHeight)) {
+							System.out.println("Смог");
+						} else {
+							System.out.println("Не Смог");
+							break;
+						}
 					} else {
-						System.out.println("Не Смог");
+						if (((Human) objects[i]).run(((Track) events[j]).trackLength)) {
+							System.out.println("Смог");
+						} else {
+							System.out.println("Не Смог");
+							break;
+						}
 					}
-				} else {
-					if (((Human) objects[i]).run(((Track) events[j]).trackLength)) {
-						System.out.println("Смог");
+					} else if (objects[i] instanceof Cat)  {
+					if (events[j] instanceof Wall) {
+						if (((Cat) objects[i]).jump(((Wall) events[j]).wallHeight)) {
+							System.out.println("Смог");
+						} else {
+							System.out.println("Не Смог");
+							break;
+						}
 					} else {
-						System.out.println("Не Смог"); }
+						if (((Cat) objects[i]).run(((Track) events[j]).trackLength)) {
+							System.out.println("Смог");
+						} else {
+							System.out.println("Не Смог");
+							break;
+						}
+					}
+				} else if (objects[i] instanceof Robot)  {
+					if (events[j] instanceof Wall) {
+						if (((Robot) objects[i]).jump(((Wall) events[j]).wallHeight)) {
+							System.out.println("Смог");
+						} else {
+							System.out.println("Не Смог");
+							break;
+						}
+					} else {
+						if (((Robot) objects[i]).run(((Track) events[j]).trackLength)) {
+							System.out.println("Смог");
+						} else {
+							System.out.println("Не Смог");
+							break;
+						}
+					}
 				}
 			}
 		}
