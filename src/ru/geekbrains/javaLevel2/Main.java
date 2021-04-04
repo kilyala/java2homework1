5c690648fb2204b302ce2660ec304b8acd9a0479
kilyala <herasav@gmail.com>
2021-04-04 20:35:50 +0300

    add smth

--- src/ru/geekbrains/javaLevel2/Main.java
+++ src/ru/geekbrains/javaLevel2/Main.java
@@ -2,36 +2,37 @@ package ru.geekbrains.javaLevel2;
 
 public class Main {
 
-    public static void main(String[] args) {
-	Human human = new Human("Bob", 200, 5);
-	Cat cat = new Cat("Tom", 100, 10);
-	Robot robot = new Robot("T800", 1000, 15);
-	Track track = new Track(150);
-	Wall wall = new Wall(8);
+	public static void main(String[] args) {
+		Human human = new Human("Bob", 200, 5);
+		Cat cat = new Cat("Tom", 100, 10);
+		Robot robot = new Robot("T800", 1000, 15);
+		Track track = new Track(150);
+		Wall wall = new Wall(8);
 
-	Object[] objects = {human, cat, robot};
-	Object[] events = {track, wall};
-
+		Object[] objects = {human, cat, robot};
+		Object[] events = {track, wall};
 
 
 		for (int i = 0; i < objects.length; i++) {
 			System.out.println("Do " + objects[i].getClass().getSimpleName());
 			for (int j = 0; j < events.length; j++) {
 				System.out.println("Met " + events[j].getClass().getSimpleName());
-				if instanceof Wall {
-					(((Human)objects[i]).jump (((Wall)events[j]).wallHeight)){
+				if (events[j] instanceof Wall) {
+					if (((Human) objects[i]).jump(((Wall) events[j]).wallHeight)) {
 						System.out.println("Смог");
 					} else {
 						System.out.println("Не Смог");
 					} else {
-						(((Human)objects[i]).jump (((Track)events[j]).trackLength)){
-							System.out.println("Смог");
-						} else {
-							System.out.println("Не Смог");
+						if (events[j] instanceof Track) {
+							if (((Human) objects[i]).jump(((Track) events[j]).trackLength)) {
+								System.out.println("Смог");
+							} else {
+								System.out.println("Не Смог");
+							}
 						}
 					}
 				}
 			}
 		}
-    }
+	}
 }
